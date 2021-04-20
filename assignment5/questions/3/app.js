f26463bc34698459f63c6e050f4a0efd4a787657
Fahad <feditechnologies@gmail.com>
2021-04-21 01:40:54 +0500

    Assignment 3 q3 done

--- assignment5/questions/3/app.js
+++ assignment5/questions/3/app.js
@@ -1,17 +1,9 @@
-var a;
-document.write("<h2>"+ "value of variable after declaraion is: "+ a +"</h2>")
+color = prompt("Enter traffic signal color")
 
-a= 10;
-document.write("<h2>"+ "Initial value is: "+ a +"</h2>")
-
-a = ++a;
-document.write("<h2>"+ "value of variable after increment is: "+ a +"</h2>")
-
-a = a+7;
-document.write("<h2>"+ "value of variable after addition is: "+ a +"</h2>")
-
-a = --a;
-document.write("<h2>"+ "value of variable after decrement is: "+ a +"</h2>")
-
-var rem = a%3;
-document.write("<h2>"+ "The remainder is: "+ rem +"</h2>")
+if(color == "red"){
+    alert("Must Stop!")
+}else if(color == "yellow"){
+    alert("Ready to Move!")
+}else if(color == "green"){
+    alert("Move Now!")
+}
