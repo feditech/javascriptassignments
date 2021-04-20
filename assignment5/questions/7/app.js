52da123444f461bb7fe51334a743d9846497fa8b
Fahad <feditechnologies@gmail.com>
2021-04-21 04:17:25 +0500

    Assignment 3 q7 done

--- assignment5/questions/7/app.js
+++ assignment5/questions/7/app.js
@@ -1,22 +1,7 @@
-var price1 = 10;
-var price2 = 20;
-var quantity1 = 5;
-var quantity2 = 4;
-var shippingfee = 10;
-
-
-var total = (price1*quantity1) + (price2*quantity2) +shippingfee;
-
-
-
-document.write("<h1>"+ "Shopping Cart "+"</h1>") 
-
-document.write("<h2>"+ "Price of item1: "+ price1 +"</h2>") 
-document.write("<h2>"+ "Price of item2: "+ price2 +"</h2>") 
-document.write("<h2>"+ "Quantity of item1: "+ quantity1 +"</h2>") 
-document.write("<h2>"+ "Quantity of item2: "+ quantity2 +"</h2>") 
-document.write("<h2>"+ "Shipping fee: "+ shippingfee +"</h2>") 
-
-
-
-document.write("<h2>"+ "Total cost: "+ total +"</h2>") 
+userinput = +prompt("Enter a number(1-10)")
+secretNumber = 5;
+if(userinput === secretNumber){
+    alert("Bingo! Correct Answer")
+}else if(++userinput === secretNumber){
+    alert("Close enough to correct answer")
+}
