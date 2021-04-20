d8caa89dffb5ecf3b39b13e0d2d041f8d9e04c8d
Fahad <feditechnologies@gmail.com>
2021-04-21 02:31:37 +0500

    Assignment 3 q6 done

--- assignment5/questions/6/app.js
+++ assignment5/questions/6/app.js
@@ -1,14 +1,60 @@
-var cel = 20;
-var far = 68;
+var Marks =100;
+var obt1 = +prompt("Enter English Marks");
+var obt2 = +prompt("Enter Urdu Marks");
+var obt3 = +prompt("Enter Math Marks");
 
-var celTofar = ( cel * 9/5) +32;
+var totalMarks = 100 * 3;
+var totalObtained = obt1+obt2+obt3;
+var percentage = (totalObtained/totalMarks)*100;
 
-var farTocel = ( far -32 ) * 5/9;
+var remark;
+var remark_A1  = "Excellent";
+var remark_A = "Excellent";
+var remark_B = "Excellent";
+var remark_F = "Fail";
+
+var grade;
+if (percentage>=80 && percentage <=100) {
+    grade="A+"
+    
+  } else if (percentage>=70 && percentage <80) {
+    grade="A"
+    
+ }
+  else if (percentage>=60 && percentage <70) {
+    grade="B"
+    
+ } 
+  else {
+    grade="F"
+    
+}
 
 
-document.write("<h2>" + "temp in celcius:" + cel  +  "</h2>")
-document.write("<h2>" + "After conversion:" + celTofar  +  "</h2>")
+if(grade === "A+")
+{
+remark = remark_A1;
+}else if(grade === "A")
+{
+remark = remark_A;
+}else if(grade === "B")
+{
+remark = remark_B;
+}else if(grade === "F")
+{
+remark = remark_F;
+} 
 
 
-document.write("<h2>" + "temp in farenheit:" + far  +  "</h2>")
-document.write("<h2>" + "After conversion:" + farTocel  +  "</h2>")
+
+document.write("<h1>"+ "Marksheet" +"</h1>")
+
+document.write("<h2>"+ "Total Marks: "+ totalMarks +"</h2>")
+
+document.write("<h2>"+ "Obtained Marks: "+ totalObtained +"</h2>")
+
+document.write("<h2>"+ "Percentage: "+ percentage +"</h2>")
+
+document.write("<h2>"+ "Grade: "+ grade +"</h2>")
+
+document.write("<h2>"+ "Remarks: "+ remark +"</h2>")
