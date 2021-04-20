0b100170f4635b79823b750286d4bd8abb51b14a
Fahad <feditechnologies@gmail.com>
2021-04-21 01:36:33 +0500

    assignment 3 q2 completed

--- assignment5/questions/2/app.js
+++ assignment5/questions/2/app.js
@@ -1,15 +1,8 @@
-var a=10;
-var b=20;
-var sub =a-b;
-var mul =a*b;
-var div =a/b;
-var mod =a%b;
+gender = prompt("Enter gender")
 
-
-
-
-
-document.write("<h2>"+"Subtraction of " + a+ " and "+ b + " is "+ sub+ "</h2>")
-document.write("<h2>"+"Multiplication of " + a+ " and "+ b + " is "+ mul+ "</h2>")
-document.write("<h2>"+"Division of " + a+ " and "+ b + " is "+ div+ "</h2>")
-document.write("<h2>"+"Modulus of " + a+ " and "+ b + " is "+ mod+ "</h2>")
+if(gender==="male"){
+    alert("Good Morning Sir.")
+}
+else if(gender==="female"){
+    alert("Good Morning Ma'am.")
+}
