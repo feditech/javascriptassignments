d06d0c9f3285647d7537e25ad6e4c52cc945abe8
Fahad <feditechnologies@gmail.com>
2021-04-21 04:21:40 +0500

    Assignment 3 q8 done

--- assignment5/questions/8/app.js
+++ assignment5/questions/8/app.js
@@ -1,30 +1,7 @@
-var totalMarks= +prompt("enter total marks");
-var obtainedMarks= +prompt("enter obtained marks");
-var percentage = (obtainedMarks/totalMarks)*100;
+userinput = +prompt("Enter a nmunber")
 
-
-document.write("<h1>"+ "Marksheet" +"</h1>")
-
-document.write("<h2>"+ "Total Marks: "+ totalMarks +"</h2>")
-
-document.write("<h2>"+ "Obtained Marks: "+ obtainedMarks +"</h2>")
-
-document.write("<h2>"+ "Percentage: "+ percentage +"</h2>")
-
-var grade;
-if (percentage>=80 && percentage <=100) {
-    grade="A+"
-  } else if (percentage>=70 && percentage <80) {
-    grade="A"
- }
-  else if (percentage>=60 && percentage <70) {
-    grade="B"
- } 
-  else if (percentage>=50 && percentage <60) {
-    grade="C"
- } 
-  else {
-    grade="F"
+if(userinput % 3 === 0 ){
+  alert("number is divisible by 3")
+}else{
+  alert("number is not divisible by 3") 
 }
-
-document.write("<h2>"+ "Grade: "+ grade +"</h2>")
