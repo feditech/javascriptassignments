f924c04b768cfde71b3f41089a4eea69ac8d503e
Fahad <feditechnologies@gmail.com>
2021-04-21 06:42:58 +0500

    Assignment 3 ch 12-13 q4 done

--- assignment6(3)/questions/4/app.js
+++ assignment6(3)/questions/4/app.js
@@ -1,1 +1,15 @@
-document.write("No question")
+var ch = prompt("enter a letter")
+var _choice;
+function checker(_choice){
+if( ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u' || ch === 'A' || ch === 'E' || ch === 'I'|| ch === 'O' || ch === 'U'){
+    return true;
+    
+}else{
+    return false;
+
+}
+
+}
+
+var result = checker(ch);
+alert(result)
