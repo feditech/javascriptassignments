03781e0d6ee1599be1eea58bfe90845d824c4647
Fahad <feditechnologies@gmail.com>
2021-04-25 12:35:35 +0500

    assignment 5 q2 done

--- assignment8(5)/questions/1 to 6/app.js
+++ assignment8(5)/questions/1 to 6/app.js
@@ -19,17 +19,3 @@ var s1 = new Student()
 console.log(s1)
 
 
-
-console.log("Q3. Declare and initialize a strings array.")
-var str = ["ali" , "usman" , "zaroon"]
-console.log(str)
-
-console.log("Q4. Declare and initialize a numbers array.")
-var numbers = [1 , 4 , 6]
-console.log(numbers)
-
-
-console.log("Q5. Declare and initialize a boolean array.")
-var bool = [true , false  ]
-console.log(bool)
-
