8d3501f0655806d47082544d2e613b4472b98a0a
Fahad <feditechnologies@gmail.com>
2021-04-29 03:59:28 +0500

    14 done

--- assignment8(5)/questions/14/app.js
+++ assignment8(5)/questions/14/app.js
@@ -1,10 +1,13 @@
-var num1 = +prompt("Enter 1st number")
-var num2 = +prompt("Enter 2nd number")
+var arr = []
 
-if( num1 === num2){
-    alert("EQUAL")
-}else if( num1 > num2){
-    alert(num1 + " is greater than "+ num2)
-}else{
-    alert(num2 + " is greater than "+ num1)
-}
+arr.push("Keyboard")
+arr.push("Mouse")
+arr.push("Printer")
+arr.push("Monitor")
+
+document.write("<h2>"+ arr +"</h2>")
+
+document.write("<h2>"+ arr.pop() +"</h2>")
+document.write("<h2>"+ arr.pop() +"</h2>")
+document.write("<h2>"+ arr.pop() +"</h2>")
+document.write("<h2>"+ arr.pop() +"</h2>")
