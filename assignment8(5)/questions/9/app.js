db7da477d3840f3529570a1a5e3f7adc2afe778b
Fahad <feditechnologies@gmail.com>
2021-04-28 12:08:25 +0500

    assignment 5 q9 a completed

--- assignment8(5)/questions/9/app.js
+++ assignment8(5)/questions/9/app.js
@@ -1,10 +1,13 @@
-var num1 = +prompt("Enter 1st number")
-var num2 = +prompt("Enter 2nd number")
+var colors = ["white","black","red"]
 
-if( num1 === num2){
-    alert("EQUAL")
-}else if( num1 > num2){
-    alert(num1 + " is greater than "+ num2)
-}else{
-    alert(num2 + " is greater than "+ num1)
-}
+document.write("<br> <br>" +" Initialized Array:")
+document.write("<h2>"+ colors +"</h2>")
+
+//TASK a Ask the user what color he/she wants to add to the 
+// beginning & add that color to the beginning of the array.Display the updated array in your browser.
+var newColor = prompt("Enter a color to add at the start")
+colors.unshift(newColor)
+
+document.write("Updated Array:")
+document.write("<h2>"+ colors +"</h2>")
+
