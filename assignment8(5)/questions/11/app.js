7c33f94a21d4883e15671de31b25bce7b5d9c203
Fahad <feditechnologies@gmail.com>
2021-04-28 20:43:25 +0500

    assignment 5 q 11 done

--- assignment8(5)/questions/11/app.js
+++ assignment8(5)/questions/11/app.js
@@ -1,10 +1,10 @@
-var num1 = +prompt("Enter 1st number")
-var num2 = +prompt("Enter 2nd number")
+var cities = ["Karachi","Lahore","Islamabad","Quetta"]
 
-if( num1 === num2){
-    alert("EQUAL")
-}else if( num1 > num2){
-    alert(num1 + " is greater than "+ num2)
-}else{
-    alert(num2 + " is greater than "+ num1)
-}
+document.write("<h2>"+ cities + "<h2>")
+
+var selectedCities = cities.slice(1,4)
+
+document.write("<h2>"+ selectedCities + "<h2>")
+
+
+
