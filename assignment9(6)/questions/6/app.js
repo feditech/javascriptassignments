dd25f6497ecdf2fb65532179a89997e18a28a026
Fahad <feditechnologies@gmail.com>
2021-05-04 01:46:35 +0400

    some changes

--- assignment9(6)/questions/6/app.js
+++ assignment9(6)/questions/6/app.js
@@ -1,60 +1,14 @@
-var Marks =100;
-var obt1 = +prompt("Enter English Marks");
-var obt2 = +prompt("Enter Urdu Marks");
-var obt3 = +prompt("Enter Math Marks");
-
-var totalMarks = 100 * 3;
-var totalObtained = obt1+obt2+obt3;
-var percentage = (totalObtained/totalMarks)*100;
-
-var remark;
-var remark_A1  = "Excellent";
-var remark_A = "Excellent";
-var remark_B = "Excellent";
-var remark_F = "Fail";
-
-var grade;
-if (percentage>=80 && percentage <=100) {
-    grade="A+"
-    
-  } else if (percentage>=70 && percentage <80) {
-    grade="A"
-    
- }
-  else if (percentage>=60 && percentage <70) {
-    grade="B"
-    
- } 
-  else {
-    grade="F"
-    
+document.write("<br>"+"Counting"+ "<br>")
+for( var i = 1; i<=10 ;i++){
+    document.write(i+ "<br>")
 }
 
+document.write("<br>"+"Reverse Counting"+ "<br>")
+for( var i = 10; i>=1 ;i--){
+    document.write(i+ "<br>")
+}
 
-if(grade === "A+")
-{
-remark = remark_A1;
-}else if(grade === "A")
-{
-remark = remark_A;
-}else if(grade === "B")
-{
-remark = remark_B;
-}else if(grade === "F")
-{
-remark = remark_F;
-} 
-
-
-
-document.write("<h1>"+ "Marksheet" +"</h1>")
-
-document.write("<h2>"+ "Total Marks: "+ totalMarks +"</h2>")
-
-document.write("<h2>"+ "Obtained Marks: "+ totalObtained +"</h2>")
-
-document.write("<h2>"+ "Percentage: "+ percentage +"</h2>")
-
-document.write("<h2>"+ "Grade: "+ grade +"</h2>")
-
-document.write("<h2>"+ "Remarks: "+ remark +"</h2>")
+document.write("<br>"+"Even"+ "<br>")
+for( var i = 2; i<=20 ;i+2){
+    document.write(i+ "<br>")
+}
