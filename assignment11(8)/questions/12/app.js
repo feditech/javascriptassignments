32d1b33f920d523da3e610f0076031f611a7e48d
Fahad <feditechnologies@gmail.com>
2021-05-31 21:14:01 +0500

    ass 8 q12 done

--- assignment11(8)/questions/12/app.js
+++ assignment11(8)/questions/12/app.js
@@ -1,8 +1,15 @@
-// Write a program that takes user input. Convert and
-// show the input in capital letters.
+// 12. Write a program that converts the variable num to
+// string.
+// var num = 35.36 ;
+// Remove the dot to display “3536” display in your browser.
 
-var input = prompt("Enter a string")
+var num = 40.65
 
+var res = num.toString()
+res = res.replace(".","")
 
-document.write("<h2>"+ "User input : "+input + "</h2>")
-document.write("<h2>"+ "Upper case "+ input.toUpperCase() + "</h2>")
+console.log(res)
+
+document.write("Num: "+num)
+
+document.write("str: "+res)
