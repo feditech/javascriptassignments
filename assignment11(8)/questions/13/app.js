c51a5e5a53e55665fa91a87422754cfb44b09ec2
Fahad <feditechnologies@gmail.com>
2021-06-02 00:59:33 +0500

    ass 8 q13 done

--- assignment11(8)/questions/13/app.js
+++ assignment11(8)/questions/13/app.js
@@ -1,8 +1,50 @@
-// Write a program that takes user input. Convert and
-// show the input in capital letters.
+// 13. Write a program to take user input and store username
+// in a variable. If the username contains any special symbol
+// among [@ . , !], prompt the user to enter a valid username.
+// For character codes of [@ .
 
-var input = prompt("Enter a string")
+// Note:
+// ASCII code of ! is 33
+// ASCII code of , is 44
+// ASCII code of . is 46
+// ASCII code of @ is 64
+
+var userInput = prompt("Enter user name")
+var check = userInput.split("")
+console.log(check)
+var i =0
+var x=0;
+while(x<check.length ){
+    if(check[i]=="!" ||check[i]==","||check[i]=="."||check[i]=="@" ){
+        alert("enter valid user name")
+        Input = prompt("Enter user name")
+        check = Input.split("")   
+        i=-1;
+        x=-1;    
+    }    
+    i++;
+    x++;
+}
+console.log("accepted")
 
 
-document.write("<h2>"+ "User input : "+input + "</h2>")
-document.write("<h2>"+ "Upper case "+ input.toUpperCase() + "</h2>")
+
+
+
+
+
+
+
+
+// for(i =j;i<check.length;i++){
+//     if(check[i]=="!" ||check[i]==","||check[i]=="."||check[i]=="@" ){
+//     alert("enter valid user name")
+//     Input = prompt("Enter user name")
+//     check = Input.split("")
+//     console.log(check)
+//     j=0;
+    
+// }
+
+// }
+// console.log("accepted")
