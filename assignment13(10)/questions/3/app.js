d17ecffd3e9b010f667735ba87bac99507d75f5f
Fahad <feditechnologies@gmail.com>
2021-06-03 00:29:26 +0500

    ass 10 q3 done

--- assignment13(10)/questions/3/app.js
+++ assignment13(10)/questions/3/app.js
@@ -1,6 +1,34 @@
-// Write a program to find the index of letter “n” in the word
-// “Pakistani” and display the result in your browser
+// Write a program that alerts the first 3 letters of the current
+// day, for example if today is Sunday then alert will show
+// Sun.
 
-var country = "Pakistani"
-document.write("<h2>"+ "String: "+ country +"</h2>")
-document.write("<h2>"+ "index of 'n' "+ country.indexOf('n')+ "</h2>")
+var date = new Date();
+
+
+
+
+
+switch (date.getDay()) {
+    case 0:
+        alert("Today is Sun")
+        break
+    case 1:
+        alert("Today is Mon")
+        break
+    case 2:
+        alert("Today is Tue")
+        break
+    case 3:
+        alert("Today is Wed")
+        break
+    case 4:
+        alert("Today is Thur")
+        break
+    case 5:
+        alert("Today is Fri")
+        break
+    case 6:
+        alert("Today is Sat")
+        break
+    
+}
