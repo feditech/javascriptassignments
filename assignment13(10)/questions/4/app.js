57548d36d4ee786b5fde32cf327b31127480e51e
Fahad <feditechnologies@gmail.com>
2021-06-03 00:36:19 +0500

    ass 10 q4 done

--- assignment13(10)/questions/4/app.js
+++ assignment13(10)/questions/4/app.js
@@ -1,6 +1,34 @@
-// Write a program to find the last index of letter “l” in the
-// word “Hello World” and display the result in your browser.
+// 4. Write a program that displays a message “It’s Fun day” if
+// its Saturday or Sunday today.
 
-var greeting = "Hello world"
-document.write("<h2>"+ "String: "+ greeting +"</h2>")
-document.write("<h2>"+ "index of 'l' is "+ greeting.lastIndexOf('l')+ "</h2>")
+
+var date = new Date();
+
+
+
+
+
+switch (date.getDay()) {
+    case 0:
+        alert("Its Fun Day")
+        break
+    case 1:
+        alert("its a working day")
+        break
+    case 2:
+        alert("its a working day")
+        break
+    case 3:
+        alert("its a working day")
+        break
+    case 4:
+        alert("its a working day")
+        break
+    case 5:
+        alert("its a working day")
+        break
+    case 6:
+        alert("Its Fun Day")
+        break
+    
+}
