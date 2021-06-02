ceb68c4350338ffd0cc51432aea621063fb7f80f
Fahad <feditechnologies@gmail.com>
2021-06-02 23:47:24 +0500

    ass 9 q8 done

--- assignment12(9)/questions/8/app.js
+++ assignment12(9)/questions/8/app.js
@@ -1,11 +1,14 @@
-// 8. Write a program to replace all occurrences of “and” in the
-// string with “&” and display the result in your browser.
-
-var message = "Ali and Sami are best friends. They play cricket and football together."
-
-var message1 = message.replace(/and/g, "&")
-
-
-
-document.write("<h2>"+ "String before replacement: "+message + "</h2>")
-document.write("<h2>"+ "String after replacement: "+message1 + "</h2>")
+// 8. Write a program that stores a random secret number from
+// 1 to 10 in a variable. Ask the user to input a number
+// between 1 and 10. If the user input equals the secret
+// number, congratulate the user.
+var ran =1;
+var userInput=2;
+while(ran!=userInput){
+    ran = Math.floor((Math.random()*10)+1)
+    userInput = +prompt("Enter a guess number between 1 and 10")    
+    if(ran===userInput){
+    alert("Congratulations!!!! You guessed the right number ")
+}else{
+    alert("try again")
+}}
