793de518fca7ca12c1aea1b7920b04c462072538
Fahad <feditechnologies@gmail.com>
2021-06-03 00:42:28 +0500

    ass 10 q5 done

--- assignment13(10)/questions/5/app.js
+++ assignment13(10)/questions/5/app.js
@@ -1,7 +1,13 @@
-// Write a program to find the character at 3rd index in the
-// word “Pakistani” and display the result in your browser.
+// 5. Write a program that shows the message “First fifteen
+// days of the month” if the date is less than 16th of the month
+// else shows “Last days of the month”.
 
+var date = new Date();
 
-var country = "Pakistani"
-document.write("<h2>"+ "String: "+ country +"</h2>")
-document.write("<h2>"+ "character at index  3 is "+ country.charAt(3) + "</h2>")
+var day = date.getDate()
+
+if(day<16){
+    alert("First fifteen days of the month")
+}else{
+    alert("Last days of the month")
+}
