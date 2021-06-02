9a73217f46b4f928d0a5c53f9ed69b7e16cedc23
Fahad <feditechnologies@gmail.com>
2021-06-02 22:32:37 +0500

    ass 8 q18 done

--- assignment11(8)/questions/18/app.js
+++ assignment11(8)/questions/18/app.js
@@ -1,8 +1,16 @@
-// Write a program that takes user input. Convert and
-// show the input in capital letters.
+// 18. You have a string “The quick brown fox jumps over the
+// lazy dog”. Write a program to count number of
+// occurrences of word “the” in given string.
 
-var input = prompt("Enter a string")
+var str = "The quick brown fox jumps over the lazy dog"
+var count =0;
 
+str = str.split(" ")
 
-document.write("<h2>"+ "User input : "+input + "</h2>")
-document.write("<h2>"+ "Upper case "+ input.toUpperCase() + "</h2>")
+for(var i = 0; i<str.length;i++){
+    if(str[i].toLowerCase()==="the"){
+        count= count +1;
+    }
+}
+
+console.log("there are total "+ count+" occurences of word 'the' ")
