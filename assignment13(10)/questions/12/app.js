2374e1670e90ab85b76db894dbb2bb6a4384a7d1
Fahad <feditechnologies@gmail.com>
2021-06-03 02:22:07 +0500

    ass 10 q12 done

--- assignment13(10)/questions/12/app.js
+++ assignment13(10)/questions/12/app.js
@@ -1,15 +1,11 @@
-// 12. Write a program that converts the variable num to
-// string.
-// var num = 35.36 ;
-// Remove the dot to display “3536” display in your browser.
+// 12. Write a program that creates a date object and show the
+// date in an alert box that is reset to 100 years back?
 
-var num = 40.65
 
-var res = num.toString()
-res = res.replace(".","")
+var date = new Date();
+console.log(date)
+var updatedDate = date.setFullYear(date.getFullYear()-100)
+updatedDate = new Date(updatedDate)
 
-console.log(res)
 
-document.write("Num: "+num)
-
-document.write("str: "+res)
+alert("100 years back it was: "+updatedDate)
