c54a2f6e7a3bb1d5df52a49a1b449927077aee15
Fahad <feditechnologies@gmail.com>
2021-06-03 17:21:29 +0500

    ass 11 q3 done

--- assignment14(11)/questions/3/app.js
+++ assignment14(11)/questions/3/app.js
@@ -1,34 +1,12 @@
-// Write a program that alerts the first 3 letters of the current
-// day, for example if today is Sunday then alert will show
-// Sun.
+// 3. Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
 
-var date = new Date();
+var n1 = +prompt("Enter first Number")
+var n2 = +prompt("Enter second Number")
+var add = sum(n1,n2)
 
+alert("Sum of "+n1+ " and "+n2+" is "+add)
 
-
-
-
-switch (date.getDay()) {
-    case 0:
-        alert("Today is Sun")
-        break
-    case 1:
-        alert("Today is Mon")
-        break
-    case 2:
-        alert("Today is Tue")
-        break
-    case 3:
-        alert("Today is Wed")
-        break
-    case 4:
-        alert("Today is Thur")
-        break
-    case 5:
-        alert("Today is Fri")
-        break
-    case 6:
-        alert("Today is Sat")
-        break
-    
+function sum(a,b){
+return a+b
 }
