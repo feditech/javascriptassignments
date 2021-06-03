b1428df9ec701980aac9a3b6c7ad410b7f40b9db
Fahad <feditechnologies@gmail.com>
2021-06-03 15:40:34 +0500

    assignment 10 q13 and q14 done

--- assignment13(10)/questions/13/app.js
+++ assignment13(10)/questions/13/app.js
@@ -2,3 +2,7 @@
 // and show his birth year in your browser.
 
 var userInput = +prompt("enter your age")
+var date = new Date();
+var currentYear= date.getFullYear();
+var birthYear = currentYear-userInput;
+alert("your birth year was "+(birthYear))
--- assignment13(10)/questions/14/app.js
+++ assignment13(10)/questions/14/app.js
@@ -1,25 +1,81 @@
-// 14. You have an array
-// A = [cake”, “apple pie”, “cookie”, “chips”, “patties”]
-// Write a program to enable “search by user input” in an
-// array. After searching, prompt the user whether the given
-// item is found in the list or not.
-// Note: Perform case insensitive search. Whether the user
-// enters cookie, Cookie, COOKIE or coOkIE, program
-// should inform about its availability.
+// 14. Write a program to generate your K-Electric bill in your
+// browser. All the amounts should be rounded off to 2
+// decimal places. Display the following fields:
+// a. Customer Name
+// b. Current Month
 
-var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+// DATE METHODS | JAVASCRIPT
 
-var userInput= prompt("welcome TO SIR GHOUS BAkery. wHAT DO you want to order? ");
-userInput = userInput.toLocaleLowerCase();
-var res;
-for(var i =0; i<A.length;i++){
-    if(userInput===A[i])
-    var res = userInput;
-}
-if(res === undefined){
-    alert(userInput+ " is not available")
-}
-else{
-    alert(userInput+ " is available")
+// Page 5 of 5
+
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date)
+// Where,
+
+// Net Amount Payable (within Due Date) = Number of units * Charges per unit
+// & Gross Amount Payable (after Due Date) = Net Amount + Late Payment Surcharge
+
+var date = new Date();
+
+var customerName = "ABC"
+var currentMonth = month(date);
+var numberOfunit = +prompt("enter no of unit"); 
+var chargesPerUnit = 16;
+var latePaymentSurcharge = 350;
+
+var netAmountPayable = numberOfunit * chargesPerUnit;
+var grossAmountPayable = numberOfunit * chargesPerUnit+latePaymentSurcharge;
+
+document.write("<h1>"+"K Elecric Bill " +"</h1>")
+document.write("<h2>"+"Customer Name: "+ customerName +"</h2>")
+document.write("<h2>"+"Month: "+currentMonth +"</h2>")
+document.write("<h2>"+"Number of units: "+numberOfunit +"</h2>")
+document.write("<br>"+"<h2>"+"Net Amount Payable (within Due Date): "+netAmountPayable +"</h2>")
+document.write("<h2>"+"Late Payment Surcharge: "+latePaymentSurcharge +"</h2>")
+document.write("<h2>"+"Gross Amount Payable (after Due Date): "+grossAmountPayable +"</h2>")
+
+
+
+
+
+
+
+
+
+
+
+
+function month(date){
+    
+
+switch (date.getMonth()) {
+    case 0:
+        return("january")
+    case 1:
+        return("Febuary")
+    case 2:
+        return("March")
+    case 3:
+        return("April")
+    case 4:
+        return("May")
+    case 5:
+        return("June")
+    case 6:
+        return("July")
+    case 7:
+        return("August")
+    case 8:
+        return("September")
+    case 9:
+        return("October")
+    case 10:
+        return("November")
+    case 11:
+        return("December")
 }
 
+}
