d56c9f67cbef70764479d444bf22193b59fb8b92
fahad <feditechnologies@gmail.com>
2021-06-04 14:23:05 -0700

    assignment 11 q7 done

--- assignment14(11)/questions/7/app.js
+++ assignment14(11)/questions/7/app.js
@@ -1,13 +1,18 @@
-// 7. Write a program that tests whether it's before noon and
-// alert “Its AM” else “its PM”.
+// 7. Write a function that take start and end number as inputs
+// & display counting in your browser.
+
+var n1 = +prompt("enter starting number")
+var n2 = +prompt("enter ending number")
 
 
-var date = new Date();
 
-var hours = date.getHours()
+document.write(counting(n1,n2))
 
-if(hours<12|| hours ===0){
-    alert("Its AM")
-}else{
-    alert("Its PM")
+function counting(a,b){
+    var arr =[],index=0 
+    for(var i = a ;i<=b;i++){
+        arr[index] = i;
+        index++;
+    }
+        return arr
 }
