7b1e9c0f424079f9c7b20da9b2ec968ca84ad46a
fahad <feditechnologies@gmail.com>
2021-06-08 13:02:01 -0700

    ass 12 q5 done

--- assignment15(12)/questions/5/app.js
+++ assignment15(12)/questions/5/app.js
@@ -1,7 +1,23 @@
-// 5. Write a function that squares its argument.
+// 5. You have learned the function indexOf. Code your own custom
+// function that will perform the same functionality. You can code
+// for single character as of now.
 
-var userInput = +prompt("enter a number")
-alert("square of "+userInput+" is "+ sqr(userInput))
-function sqr(a){
-    return a*a;
+var string = prompt("Enter a string")
+var char = prompt("Enter a character to find its index")
+var res =index(string,char) 
+if (res >= 0 && res<string.length ){
+    alert("Index of "+char+" is "+index(string,char))
+}else{
+    alert(char+" is not in the string")
+}
+
+
+function index(str , ch){
+
+    str = str.split("")    
+    for(var i=0;i<str.length;i++){
+    if(ch == str[i])
+    return i
+}
+    
 }
