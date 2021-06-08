7d2e198b5faa2574c034b5b3d13238cda1e0bdae
fahad <feditechnologies@gmail.com>
2021-06-08 11:59:47 -0700

    ass 11 q13

--- assignment14(11)/questions/13/app.js
+++ assignment14(11)/questions/13/app.js
@@ -1,4 +1,21 @@
-// 13. Write a program to ask the user about his age. Calculate
-// and show his birth year in your browser.
+// 13. Write a JavaScript function that accepts two arguments, a
+// string and a letter and the function will count the number of
 
-var userInput = +prompt("enter your age")
+// occurrences of the specified letter within the string.
+// Sample arguments : 'JSResourceS.com', 'o'
+
+var str = prompt("enter a string")
+var char = prompt("Enter a char to check its occurence")
+
+alert("String: "+str+". Occurence of "+char+" are "+charCheck(str,char))
+
+
+function charCheck(a,b){
+    var count=0
+    
+for(var i = 0;i<a.length;i++){
+    if(a[i]===b.toUpperCase()||a[i]===b.toLowerCase()){
+    count = count+1}
+}
+return count
+}
