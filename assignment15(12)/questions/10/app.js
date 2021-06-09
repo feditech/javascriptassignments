ba20f86eaaeaf4b795c020c19014688c67be97d0
fahad <feditechnologies@gmail.com>
2021-06-09 04:28:50 -0700

    ass 12 q10 done

--- assignment15(12)/questions/10/app.js
+++ assignment15(12)/questions/10/app.js
@@ -1,14 +1,21 @@
-// 10. Write a JavaScript function that checks whether a passed
-// string is palindrome or not?
-// A palindrome is word, phrase, or sequence that reads the same backward as
-// forward, e.g., madam.
+// 10. A cashier has currency notes of denominations 10, 50 and
+// 100. If the amount to be withdrawn is input through the
+// keyboard in hundreds, find the total number of currency notes
+// of each denomination the cashier will have to give to the
+// withdrawer.
 
-var userInput = prompt("Enter a string")
-palindrome(userInput)
 
-function palindrome(a) {
-    var check = a.split("").reverse().join("")
 
-    if (a === check) { alert(a + " is a palindrome") }
-    else { alert(a + " is not a palindrome") }
+var bill = +prompt("Enter amount:")
+
+
+notesDenomination(bill)
+
+
+function notesDenomination(amount){
+    document.write("<h1>Total Amount: "+amount  )
+    document.write("<h2>Required notes of Rs. 100  :  "+ Math.floor((amount / 100)) );
+	document.write("<h2>Required notes of Rs. 50   :  "+ Math.floor(((amount % 100) / 50)) );
+	document.write("<h2>Required notes of Rs. 10   :  "+ Math.floor((((amount % 100) % 50) / 10)) );
+	document.write("<h2>Amount still remaining Rs. :  "+ Math.floor((((amount % 100) % 50) % 10)) );
 }
