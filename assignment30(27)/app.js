d6c5b136f43d0b94afc0e9f8a5a4b665b7402efc
fahad <feditechnologies@gmail.com>
2021-08-21 07:36:56 +0500

    some more changes

--- assignment30(27)/app.js
+++ assignment30(27)/app.js
@@ -1,5 +1,22 @@
 
 
-let addTodo = ()=>{
-    
+let addTodo = () => {
+    let todo = document.getElementById("todo")
+    let list = document.getElementById("list-ul")
+
+    list.innerHTML += `
+    <li class="list-li">
+    <input type="text" disabled value="${todo.value}" id="todo-task">
+<button type="button" class="btn btn-primary">Edit</button>
+<button type="button" class="btn btn-danger">Delete</button>
+</li>`
+
+    firebase.database
+    console.log("fediboi109")
+
+}
+
+
+let delAll = () => {
+
 }
